355274421b7318198f3db4d7e2429f0e3e9abe11
FelipeEmos <felipe.emos.computacao@gmail.com>
2018-08-27 14:54:12 -0300

    melhor

--- teste/new/make.sh
+++
@@ -1,8 +0,0 @@
-echo "\n ----- Compile Program ----- \n"
-~/llvm/build/bin/clang++ prog.cpp -o exec -fopenacc
-
-echo "\n ------- Compile Ir ------- \n"
-~/llvm/build/bin/clang++ prog.cpp -fopenacc -S -emit-llvm
-
-# echo "\n ------- Compile AST ------- \n"
-# ~/llvm/build/bin/clang++ prog.cpp -fopenacc -Xclang -ast-dump &> dump.ast
